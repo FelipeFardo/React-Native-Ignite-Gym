20d783ecbfa58662a43741144dc1cd8c212dc69b
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-11-06 23:33:40 -0300

    feat: Background image and logo svg

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,25 @@
-import { VStack } from '@gluestack-ui/themed'
+import { VStack, Image, Center, Text } from '@gluestack-ui/themed'
+
+import BackgroundImg from '@assets/background.png'
+import Logo from '@assets/logo.svg'
 
 export function SignIn() {
-  return <VStack flex={1} bg="$gray700"></VStack>
+  return (
+    <VStack flex={1} bg="$gray700">
+      <Image
+        w="$full"
+        h={624}
+        defaultSource={BackgroundImg}
+        source={BackgroundImg}
+        alt="Pessoas treinando"
+        position="absolute"
+      />
+      <Center my="$24">
+        <Logo />
+        <Text color="$gray100" fontSize="$sm">
+          Treine sua mente e o seu corpo.
+        </Text>
+      </Center>
+    </VStack>
+  )
 }
