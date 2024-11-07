eae3535f410d307afd01cc0891e43db372688ce2
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-11-06 21:31:53 -0300

    chore: struct project and custom theme with gluestack

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  View, StatusBar } from 'react-native';
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto'
+import { Center, GluestackUIStyledProvider,Text } from '@gluestack-ui/themed';
+import { config } from'./config/gluestack-ui.config';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <GluestackUIStyledProvider config={config}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+        {fontsLoaded ? (
+          <Center flex={1} bg={'$gray700'}>
+            <Text color='red'>Home</Text>
+          </Center>
+          )
+        :(
+          <View/>
+        )}
+       </GluestackUIStyledProvider>
+  )
+}
