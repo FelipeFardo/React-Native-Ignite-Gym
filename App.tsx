903dacdcf344bc7017d263454876d978dfd5989d
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-11-06 22:56:53 -0300

    chore:: eslint config

--- App.tsx
+++ App.tsx
@@ -1,26 +1,30 @@
-import {  View, StatusBar } from 'react-native';
-import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto'
-import { Center, GluestackUIStyledProvider,Text } from '@gluestack-ui/themed';
-import { config } from'./config/gluestack-ui.config';
-import { Loading } from '@components/Loading';
+/* eslint-disable camelcase */
+
+import { StatusBar } from 'react-native'
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto'
+import { GluestackUIStyledProvider } from '@gluestack-ui/themed'
+import { config } from './config/gluestack-ui.config'
+import { Loading } from '@components/Loading'
+import { SignIn } from '@screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
+    Roboto_700Bold,
   })
 
   return (
     <GluestackUIStyledProvider config={config}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-        {fontsLoaded ? (
-          <Center flex={1} bg={'$gray700'}>
-            <Text color='red'>Home</Text>
-          </Center>
-          )
-        :(
-          <Loading/>
-        )}
-       </GluestackUIStyledProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </GluestackUIStyledProvider>
   )
 }
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,5 @@
+import { VStack } from '@gluestack-ui/themed'
+
+export function SignIn() {
+  return <VStack flex={1} bg="$gray700"></VStack>
+}
