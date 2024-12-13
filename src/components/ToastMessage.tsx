5094f60aa199ba461542b5c0e20d47381bbe3e4c
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-12-13 02:23:05 -0300

    feat: toast message component

---
+++ src/components/ToastMessage.tsx
@@ -0,0 +1,46 @@
+import {
+  ToastDescription,
+  ToastTitle,
+  Toast,
+  Pressable,
+  Icon,
+  VStack,
+} from "@gluestack-ui/themed";
+import { X } from "lucide-react-native";
+
+type Props = {
+  id: string;
+  title: string;
+  description?: string;
+  action?: "error" | "success";
+  onClose: () => void;
+};
+
+export function ToastMessage({
+  id,
+  title,
+  description,
+  action = "success",
+  onClose,
+}: Props) {
+  const bgColor = action === "success" ? "$green500" : "$red500";
+
+  return (
+    <Toast nativeID={`toast-${id}`} action={action} bgColor={bgColor} mt="$10">
+      <VStack space="xs" w="$full">
+        <Pressable alignSelf="flex-end" onPress={onClose}>
+          <Icon as={X} color="$coolGray50" size="md"></Icon>
+        </Pressable>
+
+        <ToastTitle color="$white" fontFamily="$heading">
+          {title}
+        </ToastTitle>
+        {description && (
+          <ToastDescription color="$white" fontFamily="$body">
+            {description}
+          </ToastDescription>
+        )}
+      </VStack>
+    </Toast>
+  );
+}
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,32 +1,63 @@
+import { useState } from "react";
+import { Alert, ScrollView, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Center, VStack, Text, Heading } from "@gluestack-ui/themed";
-import * as ImagePicker from "expo-image-picker";
-import { ScrollView, TouchableOpacity } from "react-native";
+import { ToastMessage } from "@components/ToastMessage";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    "https:github.com/felipefardo.png"
+  );
   async function handleUserPhotoSelect() {
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images"],
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    });
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ["images"],
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
 
-    if (photoSelected.canceled) return;
+      if (photoSelected.canceled) return;
 
-    console.log(photoSelected.assets[0]);
+      const photoURI = photoSelected.assets[0].uri;
+
+      if (photoURI) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return Alert.alert(
+            "Essa imagem é muito grande. Escolha uma de até 5MB"
+          );
+        }
+
+        setUserPhoto(photoURI);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+      <ToastMessage
+        id="1"
+        title="Mensagem de exemplo"
+        description="saasasasasac asasaa asasa"
+        action="success"
+        onClose={() => {}}
+      />
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: "https:github.com/felipefardo.png" }}
+            source={{ uri: userPhoto }}
             alt="Foto do usuário"
             size="xl"
           />
